728cd2c703fd38a83bade1239da4d96d03359a6f
march-dave <ysangy@gmail.com>
2016-04-29 07:34:01 -0700

    DB?

--- config/db.js
+++ config/db.js
@@ -3,11 +3,22 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '1234',
-  database : 'testdb'
+  host     : 'us-cdbr-azure-west-c.cloudapp.net',
+  user     : 'b2d170b20e34e4',
+  password : 'ae236d28',
+  database : 'as_8acbd97353253a1'
+  // host     : 'localhost',
+  // user     : 'root',
+  // password : '1234',
+  // database : 'testdb'
 });
+
+mysql://b2d170b20e34e4:ae236d28@us-cdbr-azure-west-c.cloudapp.net/as_8acbd97353253a1
+
+
+// Database=as_8acbd97353253a1;Data Source=us-cdbr-azure-west-c.cloudapp.net;
+// User Id=b2d170b20e34e4;Password=ae236d28
+
 
 connection.connect();
 
